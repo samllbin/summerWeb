7e1008b3d3ca796852590142e111db32b404bf92
samllbin <3057988675@qq.com>
2024-08-13 13:03:47 +0800

    push

---
+++ .history/components/carousel/index_20240812224748.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Carousel } from "antd";
+import style from "./index.module.css";
+import img1 from "../../public/QQ图片20240805123848.jpg";
+
+export default function carousel() {
+  return (
+    <Carousel autoplay adaptiveHeight arrows className={style.layout}>
+      <div>
+        {/* <h3 className={style.contentStyle}>1</h3> */}
+        <img src={img1} alt="" className={style.imgStyle} />
+      </div>
+      <div>
+        {/* <h3 className={style.contentStyle}>2</h3> */}
+        <img
+          src="../../public/QQ图片20240805123914.jpg"
+          alt=""
+          className={style.imgStyle}
+        />
+      </div>
+      <div>
+        {/* <h3 className={style.contentStyle}>3</h3> */}
+        <img
+          src="../../public/QQ图片20240808170818.jpg"
+          alt=""
+          className={style.imgStyle}
+        />
+      </div>
+      <div>
+        {/* <h3 className={style.contentStyle}>4</h3> */}
+        <img
+          src="../../public/QQ图片20240805123922.png"
+          alt=""
+          className={style.imgStyle}
+        />
+      </div>
+    </Carousel>
+  );
+}
